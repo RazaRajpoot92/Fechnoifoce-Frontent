baa5146b4ac9de0fea076473368aeccf6445c530
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-17 06:04:29 +0500

    setted blue theme in staging

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,15 @@ const Hero = () => {
       <div class=" max-w-350 relative justify-center flex flex-col md:flex-row w-full md:justify-around items-center">
         <div className="items-center md:pl-3 md:items-start md:w-1/2 pl-2 flex flex-col gap-5">
           <h1 class="  mb-2 text-2xl md:text-3xl lg:text-5xl font-bold">
-            Techno I <span className="text-yellow-500">Force</span>
+            Techno I <span className="text-blue-500">Force</span>
           </h1>
           <p className=" px-2 sm:p-0 text-sm sm:text-[16px] text-gray-300 leading-7 tracking-wider ">
-            Delivering reliable <span className="text-yellow-400">electromechanical</span> and <span className="text-yellow-400">packaging</span>  solutions that keep industries
+            Delivering reliable <span className="text-blue-500">electromechanical</span> and <span className="text-blue-500">packaging</span>  solutions that keep industries
             moving. Our expert engineers ensure efficient installation,
             maintenance, and optimization of machinery, helping your operations
             perform better, faster, and smarter every single day.
           </p>
-          <button className=" mt-5  text-lg bg-yellow-500 w-40 sm:w-50 lg:w-70 hover:bg-yellow-400 py-2 rounded-xs font-semibold active:scale-105 transition-all duration-300">
+          <button className=" mt-5  text-lg bg-blue-500 hover:cursor-pointer w-40 sm:w-50 lg:w-70 hover:bg-blue-400 py-2 rounded-xs font-semibold active:scale-105 transition-all duration-300">
             Get Free Quote
           </button>
         </div>
